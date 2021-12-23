a605b54a4126643d5171cda7a5c8ef12b1b0c05c
z0ff <ex1999@msn.com>
2021-12-24 08:27:11 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
-use std::str;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, Error, Write};
 use std::process::Stdio;
+use std::str;
 
 use redis::FromRedisValue;
 //use redis::AsyncCommands;
+use futures::prelude::*;
 use redis::streams::StreamReadReply;
 use tokio::process::Command;
 use tokio_util::codec::{FramedRead, LinesCodec};
-use futures::prelude::*;
+
+struct node {
+    hostname: String,
+    cores_total: u32,
+    cores_free: u32,
+}
 
 #[tokio::main]
-async fn main() { //-> redis::RedisResult<()> {
+async fn main() {
+    //-> redis::RedisResult<()> {
     let rd_client = redis::Client::open("redis://127.0.0.1").unwrap();
-    let mut rdconn = rd_client.get_multiplexed_async_connection().await.expect("could not estalbish redis connection");
+    let mut rdconn = rd_client
+        .get_multiplexed_async_connection()
+        .await
+        .expect("could not estalbish redis connection");
+
+    let cpu_cores: Vec<node> = Vec::new();
+
+    let nodes = match redis::cmd("SMEMBERS")
+        .arg(&["nodes"])
+        .query_async(&mut rdconn)
+        .await
+        .expect("could not execute redis command")
+    {
+        redis::Value::Bulk(a) => a,
+        _ => panic!("error"),
+    };
+
+    /*
+    for i in nodes {
+        i
+    }
+    */
 
     loop {
         let queue: StreamReadReply = redis::cmd("XREAD")
@@ -19,30 +50,27 @@ async fn main() { //-> redis::RedisResult<()> {
             .query_async(&mut rdconn)
             .await
             .expect("could not execute redis command");
-        
         let id = queue.keys[0].ids[0].id.clone();
 
-        let parallel_num: u32 = match FromRedisValue::from_redis_value(&queue.keys[0].ids[0].map["num"]) {
-            Ok(v) => {
-                match v {
+        let parallel_num: u32 =
+            match FromRedisValue::from_redis_value(&queue.keys[0].ids[0].map["num"]) {
+                Ok(v) => match v {
                     redis::Value::Data(d) => {
                         let s = str::from_utf8(&d).unwrap();
                         match s.parse() {
                             Ok(n) => n,
-                            Err(_) => continue
+                            Err(_) => continue,
                         }
                     }
                     _ => {
                         continue;
                     }
+                },
+                Err(e) => {
+                    eprintln!("Error: {:?}", e);
+                    continue;
                 }
-            }
-            Err(e) => {
-                eprintln!("Error: {:?}", e);
-                continue;
-            }
-        };
-        
+            };
         match FromRedisValue::from_redis_value(&queue.keys[0].ids[0].map["script"]) {
             Ok(v) => {
                 match v {
@@ -60,7 +88,12 @@ async fn main() { //-> redis::RedisResult<()> {
                             match line {
                                 Ok(l) => {
                                     let _: Vec<u8> = redis::cmd("XADD")
-                                        .arg(&[format!("{}_output", id), "*".to_string(), "line".to_string(), l])
+                                        .arg(&[
+                                            format!("{}_output", id),
+                                            "*".to_string(),
+                                            "line".to_string(),
+                                            l,
+                                        ])
                                         .query_async(&mut rdconn)
                                         .await
                                         .expect("could not execute redis command");
@@ -69,7 +102,6 @@ async fn main() { //-> redis::RedisResult<()> {
                                     eprintln!("Error: {:?}", e);
                                 }
                             }
-                            
                         }
                     }
                     _ => {
